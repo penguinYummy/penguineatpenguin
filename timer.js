6301eebfe6565bed65059d0d6c643c2754bd2e37
penguinYummy <a01062712541@gmail.com>
2025-10-23 12:10:26 +0900

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,1 +1,13 @@
 
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>기능 1</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h2>첫 번째 기능 화면입니다.</h2>
+    <p><a href="index.html">메인 화면으로 돌아가기</a></p>
+</body>
+</html>
